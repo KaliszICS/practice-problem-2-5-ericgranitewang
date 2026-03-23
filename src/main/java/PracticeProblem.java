3cd5a5a0434a4881dd91e818982df550367430cf
ericgranitewang <166552552+ericgranitewang@users.noreply.github.com>
2026-03-23 14:19:37 +0000

    commit real

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,5 +4,15 @@ public class PracticeProblem {
 
 	}
 
-
+	public static void insertionSort(char[] characters) {
+		for (int i = 1; i < characters.length; i++) {
+			char element = characters[i];
+			int index = i;
+			for (int j = i - 1; j >= 0 && characters[j] > element; j--) {
+				characters[j + 1] = characters[j];
+				index = j;
+			}
+			characters[index] = element;
+		}
+	}
 }
